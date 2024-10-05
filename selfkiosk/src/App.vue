44df208d7305a251e70c7ed329cbc0e2731f6cd3
Vivek Misra <115539722+vimis22@users.noreply.github.com>
2024-10-05 12:27:05 +0200

    Added a navbar with sallinggroup logos
    I have added some logos and specified that this machine pays with cash.

--- selfkiosk/src/App.vue
+++ selfkiosk/src/App.vue
@@ -1,7 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbarframe">
+    <marquee class="marquee">
+      <img class="bilka_logo" alt="bilka logo" src="./assets/bilka_logo.png">
+      <img class="bilka_logo" alt="bilka logo" src="./assets/sallinggroup_logo.png">
+      <img class="bilka_logo" alt="bilka logo" src="./assets/br_logo.png">
+      <img class="bilka_logo" alt="bilka logo" src="./assets/carlsjr_logo.png">
+      <img class="bilka_logo" alt="bilka logo" src="./assets/netto_logo.png">
+      <img class="bilka_logo" alt="bilka logo" src="./assets/fotex_logo.png">
+      <img class="bilka_logo" alt="bilka logo" src="./assets/starbucks_logo.png">
+      <!--    <router-link to="/">Home</router-link> |-->
+      <!--    <router-link to="/about">About</router-link>-->
+    </marquee>
+    <div class="payment_info">
+      Accepts only Cash and Card
+    </div>
   </nav>
   <router-view/>
 </template>
@@ -12,19 +24,35 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffff;
 }
 
-nav {
-  padding: 30px;
+.navbarframe{
+  flex-direction: row;
+  display: flex
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.marquee{
+  background-color: #82c8ff;
+  color: white;
+  height: 50px;
+  width: 75%;
+  margin-right: 10px;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.payment_info{
+  background-color: #00e362;
+  color: white;
+  height: 50px;
+  width: 18%;
+  text-align: center;
+  justify-content: center;
+}
+
+.bilka_logo{
+  height: 50px;
+  margin-left: 10px;
 }
 </style>
+<script setup lang="ts">
+</script>
