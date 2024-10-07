3da9bd8cc4bbbc1faaf237511a68b38da2bab215
Vivek Misra <115539722+vimis22@users.noreply.github.com>
2024-10-07 16:47:08 +0200

    Revert "Added a navbar with sallinggroup logos"
    This reverts commit 44df208d7305a251e70c7ed329cbc0e2731f6cd3.

--- selfkiosk/src/views/HomeView.vue
+++ selfkiosk/src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <template>
-  <HelloWorld/>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HomeContent.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
